e9f376890fa91c0915cc3ee0caf959324887c053
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-01-10 22:30:26 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Colors
+{
+    partial class ColorsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.foregroundGroupBox = new System.Windows.Forms.GroupBox();
+            this.foreWhiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.foreYellowRadioButton = new System.Windows.Forms.RadioButton();
+            this.foreRedRadioButton = new System.Windows.Forms.RadioButton();
+            this.foreBlueRadioButton = new System.Windows.Forms.RadioButton();
+            this.backgroundGroupBox = new System.Windows.Forms.GroupBox();
+            this.backWhiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.backYellowRadioButton = new System.Windows.Forms.RadioButton();
+            this.backRedRadioButton = new System.Windows.Forms.RadioButton();
+            this.backBlueRadioButton = new System.Windows.Forms.RadioButton();
+            this.applyColorButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.foregroundGroupBox.SuspendLayout();
+            this.backgroundGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // foregroundGroupBox
+            // 
+            this.foregroundGroupBox.Controls.Add(this.foreBlueRadioButton);
+            this.foregroundGroupBox.Controls.Add(this.foreRedRadioButton);
+            this.foregroundGroupBox.Controls.Add(this.foreYellowRadioButton);
+            this.foregroundGroupBox.Controls.Add(this.foreWhiteRadioButton);
+            this.foregroundGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foregroundGroupBox.Location = new System.Drawing.Point(26, 29);
+            this.foregroundGroupBox.Name = "foregroundGroupBox";
+            this.foregroundGroupBox.Size = new System.Drawing.Size(200, 146);
+            this.foregroundGroupBox.TabIndex = 0;
+            this.foregroundGroupBox.TabStop = false;
+            this.foregroundGroupBox.Text = "Foreground Color";
+            // 
+            // foreWhiteRadioButton
+            // 
+            this.foreWhiteRadioButton.AutoSize = true;
+            this.foreWhiteRadioButton.Checked = true;
+            this.foreWhiteRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foreWhiteRadioButton.Location = new System.Drawing.Point(12, 26);
+            this.foreWhiteRadioButton.Name = "foreWhiteRadioButton";
+            this.foreWhiteRadioButton.Size = new System.Drawing.Size(68, 24);
+            this.foreWhiteRadioButton.TabIndex = 0;
+            this.foreWhiteRadioButton.TabStop = true;
+            this.foreWhiteRadioButton.Text = "White";
+            this.foreWhiteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // foreYellowRadioButton
+            // 
+            this.foreYellowRadioButton.AutoSize = true;
+            this.foreYellowRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foreYellowRadioButton.Location = new System.Drawing.Point(12, 56);
+            this.foreYellowRadioButton.Name = "foreYellowRadioButton";
+            this.foreYellowRadioButton.Size = new System.Drawing.Size(73, 24);
+            this.foreYellowRadioButton.TabIndex = 1;
+            this.foreYellowRadioButton.TabStop = true;
+            this.foreYellowRadioButton.Text = "Yellow";
+            this.foreYellowRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // foreRedRadioButton
+            // 
+            this.foreRedRadioButton.AutoSize = true;
+            this.foreRedRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foreRedRadioButton.Location = new System.Drawing.Point(12, 86);
+            this.foreRedRadioButton.Name = "foreRedRadioButton";
+            this.foreRedRadioButton.Size = new System.Drawing.Size(57, 24);
+            this.foreRedRadioButton.TabIndex = 2;
+            this.foreRedRadioButton.TabStop = true;
+            this.foreRedRadioButton.Text = "Red";
+            this.foreRedRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // foreBlueRadioButton
+            // 
+            this.foreBlueRadioButton.AutoSize = true;
+            this.foreBlueRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foreBlueRadioButton.Location = new System.Drawing.Point(12, 116);
+            this.foreBlueRadioButton.Name = "foreBlueRadioButton";
+            this.foreBlueRadioButton.Size = new System.Drawing.Size(59, 24);
+            this.foreBlueRadioButton.TabIndex = 3;
+            this.foreBlueRadioButton.TabStop = true;
+            this.foreBlueRadioButton.Text = "Blue";
+            this.foreBlueRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // backgroundGroupBox
+            // 
+            this.backgroundGroupBox.Controls.Add(this.backBlueRadioButton);
+            this.backgroundGroupBox.Controls.Add(this.backRedRadioButton);
+            this.backgroundGroupBox.Controls.Add(this.backYellowRadioButton);
+            this.backgroundGroupBox.Controls.Add(this.backWhiteRadioButton);
+            this.backgroundGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backgroundGroupBox.Location = new System.Drawing.Point(232, 29);
+            this.backgroundGroupBox.Name = "backgroundGroupBox";
+            this.backgroundGroupBox.Size = new System.Drawing.Size(200, 146);
+            this.backgroundGroupBox.TabIndex = 4;
+            this.backgroundGroupBox.TabStop = false;
+            this.backgroundGroupBox.Text = "Background Color";
+            // 
+            // backWhiteRadioButton
+            // 
+            this.backWhiteRadioButton.AutoSize = true;
+            this.backWhiteRadioButton.Checked = true;
+            this.backWhiteRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backWhiteRadioButton.Location = new System.Drawing.Point(19, 26);
+            this.backWhiteRadioButton.Name = "backWhiteRadioButton";
+            this.backWhiteRadioButton.Size = new System.Drawing.Size(68, 24);
+            this.backWhiteRadioButton.TabIndex = 0;
+            this.backWhiteRadioButton.TabStop = true;
+            this.backWhiteRadioButton.Text = "White";
+            this.backWhiteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // backYellowRadioButton
+            // 
+            this.backYellowRadioButton.AutoSize = true;
+            this.backYellowRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backYellowRadioButton.Location = new System.Drawing.Point(19, 56);
+            this.backYellowRadioButton.Name = "backYellowRadioButton";
+            this.backYellowRadioButton.Size = new System.Drawing.Size(73, 24);
+            this.backYellowRadioButton.TabIndex = 1;
+            this.backYellowRadioButton.TabStop = true;
+            this.backYellowRadioButton.Text = "Yellow";
+            this.backYellowRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // backRedRadioButton
+            // 
+            this.backRedRadioButton.AutoSize = true;
+            this.backRedRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backRedRadioButton.Location = new System.Drawing.Point(19, 86);
+            this.backRedRadioButton.Name = "backRedRadioButton";
+            this.backRedRadioButton.Size = new System.Drawing.Size(57, 24);
+            this.backRedRadioButton.TabIndex = 2;
+            this.backRedRadioButton.TabStop = true;
+            this.backRedRadioButton.Text = "Red";
+            this.backRedRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // backBlueRadioButton
+            // 
+            this.backBlueRadioButton.AutoSize = true;
+            this.backBlueRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backBlueRadioButton.Location = new System.Drawing.Point(19, 116);
+            this.backBlueRadioButton.Name = "backBlueRadioButton";
+            this.backBlueRadioButton.Size = new System.Drawing.Size(59, 24);
+            this.backBlueRadioButton.TabIndex = 3;
+            this.backBlueRadioButton.TabStop = true;
+            this.backBlueRadioButton.Text = "Blue";
+            this.backBlueRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // applyColorButton
+            // 
+            this.applyColorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.applyColorButton.Location = new System.Drawing.Point(438, 73);
+            this.applyColorButton.Name = "applyColorButton";
+            this.applyColorButton.Size = new System.Drawing.Size(77, 66);
+            this.applyColorButton.TabIndex = 5;
+            this.applyColorButton.Text = "Apply Colors";
+            this.applyColorButton.UseVisualStyleBackColor = true;
+            this.applyColorButton.Click += new System.EventHandler(this.applyColorButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(22, 178);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(410, 60);
+            this.outputLabel.TabIndex = 6;
+            this.outputLabel.Text = "C Sharp";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ColorsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 266);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.applyColorButton);
+            this.Controls.Add(this.backgroundGroupBox);
+            this.Controls.Add(this.foregroundGroupBox);
+            this.Name = "ColorsForm";
+            this.Text = "Colors";
+            this.foregroundGroupBox.ResumeLayout(false);
+            this.foregroundGroupBox.PerformLayout();
+            this.backgroundGroupBox.ResumeLayout(false);
+            this.backgroundGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox foregroundGroupBox;
+        private System.Windows.Forms.RadioButton foreBlueRadioButton;
+        private System.Windows.Forms.RadioButton foreRedRadioButton;
+        private System.Windows.Forms.RadioButton foreYellowRadioButton;
+        private System.Windows.Forms.RadioButton foreWhiteRadioButton;
+        private System.Windows.Forms.GroupBox backgroundGroupBox;
+        private System.Windows.Forms.RadioButton backBlueRadioButton;
+        private System.Windows.Forms.RadioButton backRedRadioButton;
+        private System.Windows.Forms.RadioButton backYellowRadioButton;
+        private System.Windows.Forms.RadioButton backWhiteRadioButton;
+        private System.Windows.Forms.Button applyColorButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colors
+{
+    public partial class ColorsForm : Form
+    {
+        public ColorsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void applyColorButton_Click(object sender, EventArgs e)
+        {
+            if (foreWhiteRadioButton.Checked)
+            {
+                outputLabel.ForeColor = Color.White;
+            }
+            else if (foreYellowRadioButton.Checked)
+            {
+                outputLabel.ForeColor = Color.Yellow;
+            }
+            else if (foreRedRadioButton.Checked)
+            {
+                outputLabel.ForeColor = Color.Red;
+            }
+            else if (foreBlueRadioButton.Checked)
+            {
+                outputLabel.ForeColor = Color.Blue;
+            }
+            
+            if (backWhiteRadioButton.Checked)
+            {
+                outputLabel.BackColor = Color.White;
+            }
+            else if (backYellowRadioButton.Checked)
+            {
+                outputLabel.BackColor = Color.Yellow;
+            }
+            else if (backRedRadioButton.Checked)
+            {
+                outputLabel.BackColor = Color.Red;
+            }
+            else if (backBlueRadioButton.Checked)
+            {
+                outputLabel.BackColor = Color.Blue;
+            }
+        }
+    }
+}
